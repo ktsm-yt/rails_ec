595e9b819d56ba2807c4a8e2625035e96d166046
ktsm <99321574+ktsm-yt@users.noreply.github.com>
2025-04-21 21:04:41 +0900

    feat Productコントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'products/index'
+  get 'products/show'
   resources :tasks
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
