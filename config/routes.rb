3dad20d736b5ebebbcfc2c1c97972f80d711cd00
ktsm <99321574+ktsm-yt@users.noreply.github.com>
2025-04-21 22:07:46 +0900

    feat 顧客用スコープを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :products
+  
   root to: 'products#index'
+
+  scope module: :customer do
+    resources :products
+  end
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
