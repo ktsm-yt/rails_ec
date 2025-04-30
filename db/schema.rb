41880feb959b3c6aab1c947505838675fe2365b9
ktsm <99321574+ktsm-yt@users.noreply.github.com>
2025-04-30 18:44:57 +0900

    feat Cart model & table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_04_22_142732) do
+ActiveRecord::Schema[7.0].define(version: 2025_04_30_093836) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,6 +40,13 @@ ActiveRecord::Schema[7.0].define(version: 2025_04_22_142732) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "carts", force: :cascade do |t|
+    t.string "session_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["session_id"], name: "index_carts_on_session_id", unique: true
   end
 
   create_table "products", force: :cascade do |t|
