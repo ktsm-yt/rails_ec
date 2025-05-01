4a9dfa23a222f91853512fadfad487ed24e4fdd7
ktsm <99321574+ktsm-yt@users.noreply.github.com>
2025-05-01 15:39:22 +0900

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,9 +10,11 @@ Rails.application.routes.draw do
 
   scope module: :customer do
     resources :products, only: %i[index show]
+    resources :cart, only: [:index]
+    post 'carts/add_item/:product_id',to: 'carts#add_item', as: :add_item
+    patch 'carts/update_item/:id', to: 'carts#update_item', as: :update_item
+    delete 'cart/remove_item/:id', to: 'carts#remove_item', as: :remove_item
+    delete 'cart', to: 'carts#destroy', as: :empty_cart
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
