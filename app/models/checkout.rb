28a5e9d6c44bdc5328c8397d8b32476bfdcaaf46
ktsm <99321574+ktsm-yt@users.noreply.github.com>
2025-05-11 21:12:40 +0900

    feat Checkout model

--- app/models/checkout.rb
+++ app/models/checkout.rb
@@ -1,4 +1,11 @@
 class Checkout < ApplicationRecord
   belongs_to :countries
   belongs_to :state
+  has_one :credit_card, dependent: :destroy
+
+  # CreditCardの属性をCheckoutフォームからまとめて受け取れるようにする
+  accept_nested_attributes_for :credit_card
+
+  # フォームで関連オブジェクトを作成するために必要
+  validates_associated :credit_card
 end
--- app/models/credit_card.rb
+++ app/models/credit_card.rb
@@ -1,2 +1,10 @@
 class CreditCard < ApplicationRecord
+  belongs_to :checkout
+
+  with_options presence: true do
+    validates :name_on_card
+    validates :card_number, numericality:{only_integer: true}
+    validates :expiration_month, numericality: {only_integer: true}
+    validates :expiration_year, numericality: {only_integer: true}
+  end
 end
