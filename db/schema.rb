21c360ad15404c3541d84bdab399222484ba4c1f
ktsm <99321574+ktsm-yt@users.noreply.github.com>
2025-05-11 10:05:59 +0900

    feat add Country & State table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_04_30_100400) do
+ActiveRecord::Schema[7.0].define(version: 2025_05_11_005651) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -59,6 +59,13 @@ ActiveRecord::Schema[7.0].define(version: 2025_04_30_100400) do
     t.index ["session_id"], name: "index_carts_on_session_id", unique: true
   end
 
+  create_table "countries", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_countries_on_name", unique: true
+  end
+
   create_table "products", force: :cascade do |t|
     t.string "name", null: false
     t.text "description", null: false
@@ -70,6 +77,13 @@ ActiveRecord::Schema[7.0].define(version: 2025_04_30_100400) do
     t.string "image_url"
     t.boolean "on_sale"
     t.integer "rating"
+  end
+
+  create_table "states", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_states_on_name", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
