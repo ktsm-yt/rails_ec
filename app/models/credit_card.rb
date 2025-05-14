17043d22b0bfc7c194ef3132c8d05f7641edf0df
ktsm <99321574+ktsm-yt@users.noreply.github.com>
2025-05-14 17:44:45 +0900

    feat validation for credit_card

--- app/models/credit_card.rb
+++ app/models/credit_card.rb
@@ -3,7 +3,7 @@ class CreditCard < ApplicationRecord
 
   with_options presence: true do
     validates :name_on_card
-    validates :card_number, numericality: { only_integer: true }
+    validates :card_number, numericality: { only_integer: true }, length: {is: 16}
     validates :expiration_month, numericality: { only_integer: true }
     validates :expiration_year, numericality: { only_integer: true }
   end
