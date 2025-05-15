2f955b7d4214f4aa27a5917a24b0d9b49286105c
ktsm <99321574+ktsm-yt@users.noreply.github.com>
2025-05-15 18:18:31 +0900

    fix according to rubocop rules

--- app/controllers/customer/checkouts_controller.rb
+++ app/controllers/customer/checkouts_controller.rb
@@ -11,7 +11,7 @@ class Customer::CheckoutsController < ApplicationController
     @checkout = @current_cart.build_checkout(checkout_params)
 
     if @checkout.save
-      
+
       PurchaseMailer.purchase_confirmation_email(@checkout).deliver_later
 
       redirect_to products_path, notice: '購入ありがとうございます'
