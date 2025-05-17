4a37cf66d8f40714f46ebf53ab37f3c22c835dc7
ktsm <99321574+ktsm-yt@users.noreply.github.com>
2025-05-17 17:46:35 +0900

    update method update_item ∵ session data will be lost

--- app/controllers/customer/cart_controller.rb
+++ app/controllers/customer/cart_controller.rb
@@ -8,18 +8,17 @@ class Customer::CartController < ApplicationController
 
     @united_states = Country.find_by(name: 'United States')
     @california = State.find_by(name: 'California', country: @united_states)
-    # 中身がnilでも必ず配列を作成する
-    @countries = [@united_states].compact
+    @countries = [@united_states].compact # 中身がnilでも必ず配列を作成する
     @states = [@california].compact
 
     # もしフォームにデータがあれば読み込む
     if session[:checkout_params]
       @checkout = Checkout.new(session[:checkout_params])
       # 一度利用したsessionを無駄に保持するとエラーの温床なのでクリアして更新に備える
-      session.delete(:checkout_params)
+      # session.delete(:checkout_params)
     else
       @checkout = Checkout.new
-      @checkout.build_credit_card # has_oneのときの書き方。普通は.build
+      @checkout.build_credit_card # has_oneのときの書き方。has_manyは.build
     end
     # なぜかリダイレクト後にpayment以下が消えちゃう
     # CreditCardオブジェクトが存在しない場合にビルド
@@ -42,7 +41,7 @@ class Customer::CartController < ApplicationController
     quantity = params[:quantity].to_i
 
     if quantity.positive?
-      # 数量更新に変換したquantityを使用 キーワード引数省略
+      # 数量更新に上記で変換したquantityを使用 キーワード引数省略
       if @cart_item.update(quantity:)
         flash[:notice] = '数量を更新しました'
       else
@@ -52,6 +51,14 @@ class Customer::CartController < ApplicationController
     else
       @cart_item.destroy
       flash[:notice] = 'カートから商品を削除しました'
+    end
+
+    checkout_data = extract_checkout_data_from_params
+    session[:checkout_params] = checkout_data if checkout_data.present?
+    # 数量更新に成功/失敗、あるいはアイテム削除された後
+    # チェックアウトフォームの入力内容を保持するため、セッションに保存する
+    if params[:checkout].present?
+      session[:checkout_params] = checkout_data
     end
 
     redirect_to cart_path
@@ -78,5 +85,36 @@ class Customer::CartController < ApplicationController
   # Product.find または CartItem.find で ActiveRecord::RecordNotFound が発生した場合
   def handle_record_not_found
     redirect_to cart_path, alert: '指定されたアイテムは見つかりませんでした'
+  end
+
+  def extract_checkout_data_from_params
+    checkout_data = {}
+    
+    # 基本フィールドの抽出
+    [:first_name,
+     :last_name,
+     :username,
+     :email,
+     :address1,
+     :address2,
+     :country_id,
+     :state_id,
+     :zip,
+     :shipping_same_as_billing,
+     :save_info_for_next_time].each do |field|
+      checkout_data[field] = params[field] if params[field].present?
+    end
+    
+    # クレジットカード情報の抽出
+    if params[:card_number].present?
+      checkout_data[:credit_card_attributes] = {
+        name_on_card: params[:name_on_card],
+        card_number: params[:card_number],
+        expiration_month: params[:expiration_month],
+        expiration_year: params[:expiration_year]
+      }.compact
+    end
+    
+    checkout_data
   end
 end
