c5aa8d3996e52bedccac04d0c661d014f472cd7f
ktsm <99321574+ktsm-yt@users.noreply.github.com>
2025-05-24 00:26:11 +0900

    refactor railsらしい美しさ

--- app/controllers/customer/cart_controller.rb
+++ app/controllers/customer/cart_controller.rb
@@ -13,23 +13,28 @@ class Customer::CartController < ApplicationController
   # current_cartは現在のユーザーセッションに関連付けられているCartのobj
   def add_item
     product = Product.find(params[:product_id]) # Product.objを見つけ
-    quantity = params[:quantity].to_i.positive? ? params[:quantity].to_i : 1 # nilなら1に
+    quantity = [params[:quantity].to_i,1].max # -,0でも1に
 
-    @current_cart.add_product(product, quantity) # 数量を渡してCartクラスのadd_productを呼び出し
-    @current_cart.save # DBへ保存し,カートを離れても有効に
-    redirect_to root_path, notice: "「#{product.name}」をカートに追加しました"
+    @current_cart.add_product(product, quantity) # Cartより
+    if @current_cart.save # DBへ保存し,カートを離れても有効に
+      redirect_to root_path, notice: "「#{product.name}」をカートに追加しました"
+    else
+      redirect_to root_path, alert: 
   end
 
   # カートの数値の直接更新
   def update_item
-    quantity = params[:quantity].to_i
-    message, flash_type, operation_succeeded = set_update_status(@cart_item.update(quantity:))
-    render_cart_response(message, flash_type, operation_succeeded)
+    if @cart_item.update(cart_item_params)
+      handle_successful_cart_operation()
+    else
+      handle_failed_cart_operation(@cart_item.errors.full_messages.join(', '))
+    end
   end
 
   def remove_item
-    message, flash_type, operation_succeeded = set_remove_status(@cart_item.destroy)
-    render_cart_response(message, flash_type, operation_succeeded)
+    handle_successful_cart_operation()
+    else
+    handle_failed_cart_operation()
   end
 
   def destroy
@@ -59,48 +64,28 @@ class Customer::CartController < ApplicationController
     redirect_to cart_path, alert: '指定されたアイテムは見つかりませんでした'
   end
 
-  def handle_cart_item_update(quantity)
-    message, flash_type, operation_succeeded = set_update_status(@cart_item.update(quantity:))
-    render_cart_response(message, flash_type, operation_succeeded)
-  end
-
-  # カートアイテム削除時のステータスを設定
-  def set_remove_status(success)
-    if success
-      ['カートから商品を削除しました', :notice, true]
-    else # 削除失敗
-      ['商品の削除に失敗しました', :alert, false]
-    end
-  end
-
-  # カートアイテム更新時のステータスを設定
-  def set_update_status(success)
-    if success
-      ['数量を更新しました', :notice, true]
-    else # 更新失敗
-      [@cart_item.errors.full_messages.join(', '), :alert, false]
-    end
-  end
-
-
-  # カート操作後のレスポンスをレンダリング
-  def render_cart_response(message, flash_type, operation_succeeded)
-    flash.now[flash_type] = message
-    load_cart_items if operation_succeeded
+  def handle_successful_cart_operation(message)
+    load_cart_items
 
     respond_to do |format|
-      if @current_cart.cart_items.empty? && operation_succeeded
-        format.html { redirect_to products_path, notice: message }
-      else
-        format.html { redirect_to cart_path, flash_type => message }
-      end
-
-      if operation_succeeded
-        format.turbo_stream
-      else # 更新失敗
-        format.turbo_stream do
-          render turbo_stream: turbo_stream.update('flash-messages', partial: 'shared/flash_messages')
+      format.html do
+        if @current_cart.cart_items.empty?
+          redirect_to products_path, notice: message
+        else
+          redirect_to cart_path, noticeL message
         end
+      end
+      format.turbo_stream
+    end
+  end
+
+  def handle_failed_cart_operation(error_message)
+    respond_to do |format|
+      format.html { redirect_to products_path, alert: message }
+      format.turbo_stream do
+        flash.now[:alert] = error_message
+        render turbo_stream: turbo_stream.update('flash-messages',
+                                                partial: 'shared/flash_messages')
       end
     end
   end
