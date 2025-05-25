32bc76282439189d90a1c32734cbb062fc043cc3
ktsm <99321574+ktsm-yt@users.noreply.github.com>
2025-05-25 13:03:23 +0900

    feat nav cart count

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,15 @@
 
 class ApplicationController < ActionController::Base
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  before_action :set_current_cart
+  before_action :set_cart_items
 
   def set_current_cart
     @current_cart = Cart.find_or_create_by(session_id: session.id.to_s)
+  end
+
+  def set_cart_items
+    @cart_items = @current_cart&.cart_items || [] # nilの場合は[]となり,[].sumで0が表示される。
   end
 
   private
